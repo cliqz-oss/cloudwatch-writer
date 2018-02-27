e82535099ce60bdbd473943ee1478e39213fc5ed
Dhananjay Balan <dhananjay@cliqz.com>
2018-02-27 16:33:40 +0100

    Intial functionality complete.

--- prom_cloudwatch_writer/promcloudwatch.go
+++ prom_cloudwatch_writer/promcloudwatch.go
@@ -1,5 +1,6 @@
-// prom-cloudwatch-remote-writer writes incoming metrics from prometheus
-// (configured using remote_write config) to cloudwatch.
+// prom_cldwatch_writer implements http server listner and a batch upload
+// mechanism to cloudwatch. AWS credentials as read in aws-sdk-go, from either
+// one of Environment Variable|awsconfig file|IAM role
 package prom_cldwatch_writer
 
 import (
@@ -21,6 +22,12 @@ var (
 	debug = false
 )
 
+func debugPrint(msg string) {
+	if debug {
+		fmt.Fprintf(os.Stderr, "%s\n", msg)
+	}
+}
+
 // StartMetricExporter starts listening on serverAddr and will export metrics posted to Cloudwatch
 // with namespace and region awsRegion
 func StartMetricExporter(serverAddr, namespace, awsRegion string, verbose bool) error {
@@ -28,7 +35,8 @@ func StartMetricExporter(serverAddr, namespace, awsRegion string, verbose bool) 
 	conn := cloudwatch.New(session.New(), &aws.Config{Region: aws.String(awsRegion)})
 	tsQueue := make(chan *prompb.TimeSeries, 10)
 	go writeToCloudWatch(conn, tsQueue, namespace)
-	fmt.Fprintf(os.Stderr, "listening on: %s\n", serverAddr)
+
+	debugPrint("listening on: " + serverAddr)
 	return runHTTPServer(serverAddr, tsQueue)
 }
 
@@ -75,6 +83,7 @@ func getMetricDatum(ts *prompb.TimeSeries) ([]*cloudwatch.MetricDatum, error) {
 func writeToCloudWatch(conn *cloudwatch.CloudWatch, tsQueue <-chan *prompb.TimeSeries, namespace string) {
 	ticker := time.NewTicker(5 * time.Second)
 	datums := []*cloudwatch.MetricDatum{}
+
 	for {
 		select {
 		case ts := <-tsQueue:
@@ -84,23 +93,35 @@ func writeToCloudWatch(conn *cloudwatch.CloudWatch, tsQueue <-chan *prompb.TimeS
 				break
 			}
 
+			if len(datumList) <= 0 {
+				fmt.Fprintf(os.Stderr, "error: emtpy datum!")
+				break
+			}
+
 			for _, d := range datumList {
 				datums = append(datums, d)
 			}
+
 		case <-ticker.C:
+			if len(datums) == 0 {
+				break
+			}
+
+			debugPrint(fmt.Sprintf("sending %d datapoints to cw", len(datums)))
+
 			metricData := &cloudwatch.PutMetricDataInput{
 				Namespace:  &namespace,
 				MetricData: datums,
 			}
 
-			if debug {
-				fmt.Printf("writing to cw: %v\n", metricData)
-			}
-
+			debugPrint(fmt.Sprintf("writing to cw: %v\n", metricData))
 			_, err := conn.PutMetricData(metricData)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "error: writing to cw: %v\n", err)
 			}
+
+			// reset datum to send
+			datums = []*cloudwatch.MetricDatum{}
 		}
 	}
 }
@@ -110,21 +131,21 @@ func runHTTPServer(addr string, tsQueue chan<- *prompb.TimeSeries) error {
 		compressed, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-			if debug {
-				fmt.Fprintf(os.Stderr, "request failed: %v\n", err)
-			}
+			debugPrint("request failed: " + err.Error())
 			return
 		}
 
 		reqBuf, err := snappy.Decode(nil, compressed)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
+			debugPrint("request failed: " + err.Error())
 			return
 		}
 
 		var req prompb.WriteRequest
 		if err := proto.Unmarshal(reqBuf, &req); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
+			debugPrint("unmarshal failed: " + err.Error())
 			return
 		}
 
