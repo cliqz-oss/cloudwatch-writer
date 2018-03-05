54dfec14d753ff1b825f31c11154bc6c46018e92
Dhananjay Balan <dhananjay@cliqz.com>
2018-03-05 14:03:25 +0100

    Project will be released under MPL.

--- prom_cloudwatch_writer/promcloudwatch.go
+++ prom_cloudwatch_writer/promcloudwatch.go
@@ -1,3 +1,9 @@
+// Copyright © 2018 Dhananjay Balan <dhananjay@cliqz.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 // prom_cldwatch_writer implements http server listner and a batch upload
 // mechanism to cloudwatch. AWS credentials as read in aws-sdk-go, from either
 // one of Environment Variable|awsconfig file|IAM role
